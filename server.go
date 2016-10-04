fda29cf3af1c7c97d3e0055b7978a62c88e572c5
Zeu Cxb <zeucxb@MacBook-Pro-de-Zeu.local>
2016-10-04 16:12:41 -0300

    add go server

---
+++ server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./"))
+	http.Handle("/", fs)
+
+	log.Println("Listening...")
+	http.ListenAndServe(":4747", nil)
+}
