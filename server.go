a8cd492e46b128dd57d21d56f8195cfd4fe7eceb
Zeu Cxb <zeucxb@MacBook-Pro-de-Zeu.local>
2016-10-04 16:43:45 -0300

    ENV validation

--- server.go
+++ server.go
@@ -8,10 +8,17 @@ import (
 
 func main() {
 	var home = os.Getenv("HOME")
+	if home == "" {
+		home = "./"
+	}
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = "4747"
+	}
 
 	fs := http.FileServer(http.Dir(home + "/public/"))
 	http.Handle("/", fs)
 
 	log.Println("Listening...")
-	http.ListenAndServe(":4747", nil)
+	http.ListenAndServe(":"+port, nil)
 }
